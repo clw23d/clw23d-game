4883d66ebdecdb2c020045b1e35f8e97d0b42485
brownbookcover <user@FIH-COM23a105.fsu.edu>
2024-09-20 14:42:40 -0400

    fixed ball code

--- Assets/ball_collision.cs
+++ Assets/ball_collision.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class ball_collision : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // instantiate variables
+    private Vector3 initialPosition;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        //store the initial location of the sphere
+        initialPosition = transform.position;
+        // get the rigidbody component
+        rb = GetComponent<Rigidbody>();
     }
+
+    // when object collides, reset the position to that saved var
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Plane")
         {
-            gameObject.SetActive(false);
+            //set the sphere's position to the initial position
+            transform.position = initialPosition;
+            //set velocity and angular velocity to zero
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+        }
+        else if (collision.gameObject.name == "Goal")
+        {
+            Debug.Log("a winner is you!");
         }
         else
         {
             Debug.Log("nothing happened");
-         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
